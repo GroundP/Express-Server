706efa479234cceebb3a2522a3a68461ed841780
groundp <bloom0819@naver.com>
2022-07-07 00:54:47 +0900

    use middleware, router on express

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
-
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-    //res.send('Hello, Express');
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
